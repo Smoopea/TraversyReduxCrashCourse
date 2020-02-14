a496763e877b62708891528924bac746ada502c1
blakegeguzis <blakegeguzis@gmail.com>
2020-02-14 15:24:20 -0600

    The final touches on the project ^^

--- src/components/Postform.js
+++ src/components/Postform.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { createPost} from '../actions/postActions';
 
-class Postform extends Component {
+class PostForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -24,15 +27,7 @@ class Postform extends Component {
        body: this.state.body
     }
 
-    fetch('https://jsonplaceholder.typicode.com/posts',{
-      method:'POST',
-      headers: {
-        'content-type': 'application/json'
-      },
-      body: JSON.stringify(post)
-    })
-    .then(res => res.json())
-    .then(data => console.log(data));
+    this.props.createPost(post);
   }
 
   render() {
@@ -59,4 +54,8 @@ class Postform extends Component {
   }
 }
 
-export default Postform
+PostForm.propTypes = {
+  createPost: PropTypes.func.isRequired
+}
+
+export default connect(null,{ createPost })(PostForm);
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import {fetchPosts } from '../actions/postActions';
 
 class Posts extends Component {
   componentWillMount() {
     this.props.fetchPosts();
+  }
+  componentWillReceiveProps(nextProps) {
+    if(nextProps.newPost) {
+      this.props.posts.unshift(nextProps.newPost);
+    }
   }
 
   render() {
@@ -22,9 +28,15 @@ class Posts extends Component {
     )
   }
 }
+Posts.propTypes = {
+  fetchPosts: PropTypes.func.isRequired,
+  posts: PropTypes.array.isRequired,
+  newPost: PropTypes.object.isRequired
+}
 
 const mapStateToProps = state => ({
-  posts: state.posts.items
+  posts: state.posts.items,
+  newPost: state.posts.item
 })
 
 export default connect(mapStateToProps,{ fetchPosts })(Posts);
