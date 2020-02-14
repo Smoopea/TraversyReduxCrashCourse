5f6d4ea4cb3c7af0a6ed2aa2f8a95e2d16ddcbd8
blakegeguzis <blakegeguzis@gmail.com>
2020-02-13 22:21:51 -0600

    Added a mock Redux store

--- src/App.js
+++ src/App.js
@@ -3,17 +3,24 @@ import logo from './logo.svg';
 import Posts from './components/Posts';
 import Postform from './components/Postform';
 import './App.css';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+const store = createStore(() => [], {}, applyMiddleware())
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Postform />
-        <hr />
-        <Posts />
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <Postform />
+          <hr />
+          <Posts />
+        </div>
+      </Provider>
     );
   }
 }
 
 export default App;
+//Do I need to
